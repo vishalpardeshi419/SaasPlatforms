7481b51726d7aace472855a203ee7c492262a653
vishal <vpardeshi419@gmail.com>
2024-05-03 22:21:12 +0530

    Removed All files

--- backend/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'https://www.google.com',
-    'http://localhost:3500',
-    'http://127.0.0.1:5500',
-    'http://localhost:3000/'
-]
-
-module.exports = allowedOrigins;
--- backend/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const logEvents = require('./logger')
-
-const errorHandler = (err, req, res, next) => {
-    logEvents(`${err.name}: ${err.message}`, 'error.txt');
-    console.log(err.stack)
-    res.status(500).send(err.message);
-}
-
-module.exports = errorHandler;
